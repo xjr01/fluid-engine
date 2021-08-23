48eed033f7b858af01a7d7ed3bfbfcbaebe3c8d3
xjr01 <1900012131@pku.edu.cn>
2021-08-23 22:30:28 +0800

    添加 Triangle 类

--- Geometries/Triangle.cpp
+++ Geometries/Triangle.cpp
@@ -1,4 +1,8 @@
+#include <Eigen/Geometry>
+#include <limits>
 #include "Triangle.h"
+#include <Utilities/MathFunctions.h>
+#include <Utilities/Constants.h>
 
 _FLUID_ENGINE_BEGIN
 
@@ -11,29 +15,112 @@ inline Triangle<Dim>::Triangle(
 	_normals(normals),
 	_uvs(uvs)
 {
+	try {
+		for (int i = 0; i < 3; ++i)
+			MathFunc::normalize<VectorDr>(_normals[i]);
+	}
+	catch (const char* msg) {
+		if (!strcmp(msg, "Vector being normalized should not be zero!"))
+			throw "Normal should not be zero!";
+		throw msg;
+	}
+}
+
+template<int Dim>
+Triangle<Dim>::VectorDr Triangle<Dim>::faceNormal() const
+{
+	try {
+		return MathFunc::normalized<VectorDr>((_points[1] - _points[0]).cross(_points[2] - _points[0]));
+	}
+	catch (const char* msg) {
+		if (!strcmp(msg, "Vector being normalized should not be zero!"))
+			throw "Three points of a triangle must distinct from each other!";
+		throw msg;
+	}
+}
+
+template<int Dim>
+real Triangle<Dim>::area() const
+{
+	return (real).5 * (_points[1] - _points[0]).cross(_points[2] - _points[0]).norm();
+}
+
+template<int Dim>
+void Triangle<Dim>::setNormalsToFaceNormal()
+{
+	_normals[0] = _normals[1] = _normals[2] = faceNormal();
 }
 
 template<int Dim>
 Triangle<Dim>::VectorDr Triangle<Dim>::closestPoint(const VectorDr& otherPoint) const
 {
-	return VectorDr();
+	VectorDr normal = faceNormal();
+	real signedDistanceToPlane = (otherPoint - _points[0]).dot(normal);
+	VectorDr projectedPoint = otherPoint - signedDistanceToPlane * normal;
+
+	if ((_points[1] - _points[0]).cross(projectedPoint - _points[0]).dot(normal) < 0)
+		return closestPointOnEdge(_points[0], _points[1], otherPoint);
+	if ((_points[2] - _points[1]).cross(projectedPoint - _points[1]).dot(normal) < 0)
+		return closestPointOnEdge(_points[1], _points[2], otherPoint);
+	if ((_points[0] - _points[2]).cross(projectedPoint - _points[2]).dot(normal) < 0)
+		return closestPointOnEdge(_points[2], _points[0], otherPoint);
+	return projectedPoint;
 }
 
 template<int Dim>
 Triangle<Dim>::VectorDr Triangle<Dim>::closestNormal(const VectorDr& otherPoint) const
 {
-	return VectorDr();
+	VectorDr normal = faceNormal();
+	real signedDistanceToPlane = (otherPoint - _points[0]).dot(normal);
+	VectorDr projectedPoint = otherPoint - signedDistanceToPlane * normal;
+
+	if ((_points[1] - _points[0]).cross(projectedPoint - _points[0]).dot(normal) < 0)
+		return closestNormalOnEdge(_points[0], _points[1], _normals[0], _normals[1], otherPoint);
+	if ((_points[2] - _points[1]).cross(projectedPoint - _points[1]).dot(normal) < 0)
+		return closestNormalOnEdge(_points[1], _points[2], _normals[1], _normals[2], otherPoint);
+	if ((_points[0] - _points[2]).cross(projectedPoint - _points[2]).dot(normal) < 0)
+		return closestNormalOnEdge(_points[2], _points[0], _normals[2], _normals[0], otherPoint);
+	return normal;
 }
 
 template<int Dim>
 BoundingBox<Dim> Triangle<Dim>::boundingBox() const
 {
-	return BoundingBox<Dim>();
+	BoundingBox<Dim> bound(_points[0], _points[1]);
+	bound.merge(_points[2]);
+	return bound;
 }
 
 template<int Dim>
 void Triangle<Dim>::getClosestIntersection(const Ray<Dim>& ray, SurfaceRayIntersection<Dim>& intersection) const
 {
+}
+
+template<int Dim>
+Triangle<Dim>::VectorDr Triangle<Dim>::closestPointOnEdge(
+	const VectorDr& p1, const VectorDr& p2, const VectorDr& otherPoint) const
+{
+	real lengthSquared = (p2 - p1).squaredNorm();
+	if (lengthSquared <= MathFunc::sqr(eps))
+		return p1;
+	real t = (otherPoint - p1).dot(p2 - p1) / lengthSquared;
+	if (t < 0) return p1;
+	if (t > 1) return p2;
+	return p1 + t * (p2 - p1);
+}
+
+template<int Dim>
+Triangle<Dim>::VectorDr Triangle<Dim>::closestNormalOnEdge(
+	const VectorDr& p1, const VectorDr& p2,
+	const VectorDr& n1, const VectorDr& n2, const VectorDr& otherPoint) const
+{
+	real lengthSquared = (p2 - p1).squaredNorm();
+	if (lengthSquared <= MathFunc::sqr(eps))
+		return p1;
+	real t = (otherPoint - p1).dot(p2 - p1) / lengthSquared;
+	if (t < 0) return n1;
+	if (t > 1) return n2;
+	return MathFunc::normalized<VectorDr>(n1 + t * (n2 - n1));
 }
 
 template class Triangle<3>;
--- Geometries/Triangle.h
+++ Geometries/Triangle.h
@@ -10,8 +10,15 @@ class Triangle : public Surface<Dim> {
 	static_assert(Dim == 3, "Dimension of Triangle must be 3");
 	DECLARE_DIM_TYPES(Dim)
 
+	/*
+	* Three points of this triangle
+	* note: the order decides the direction of the face normal
+	*	Right hand law according to _points[0] -> _points[1] -> _points[2]
+	*/
 	std::array<VectorDr, 3> _points;
+	/* Normals at the three points */
 	std::array<VectorDr, 3> _normals;
+	/* Uv coordinates */
 	std::array<Vector2r, 3> _uvs;
 public:
 	Triangle(
@@ -23,10 +30,25 @@ public:
 	const std::array<VectorDr, 3>& normals() const { return _normals; }
 	const std::array<Vector2r, 3>& uvs() const { return _uvs; }
 
+	/* Returns the face normal of this triagle */
+	VectorDr faceNormal() const;
+	/* Returns the area of this triangle */
+	real area() const;
+	/* Set the three normals to the face normal */
+	void setNormalsToFaceNormal();
+
 	virtual VectorDr closestPoint(const VectorDr& otherPoint) const override;
 	virtual VectorDr closestNormal(const VectorDr& otherPoint) const override;
 	virtual BoundingBox<Dim> boundingBox() const override;
 	virtual void getClosestIntersection(const Ray<Dim>& ray, SurfaceRayIntersection<Dim>& intersection) const override;
+private:
+	/* Helper functions */
+
+	/* Returns the closest point on the edge (p1, p2) to given point */
+	VectorDr closestPointOnEdge(const VectorDr& p1, const VectorDr& p2, const VectorDr& otherPoint) const;
+	/* Returns the closest normal on the edge (p1, p2) (with normals n1 and n2) to given point */
+	VectorDr closestNormalOnEdge(const VectorDr& p1, const VectorDr& p2,
+		const VectorDr& n1, const VectorDr& n2, const VectorDr& otherPoint) const;
 };
 
 _FLUID_ENGINE_END
